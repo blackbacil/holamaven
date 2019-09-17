54e0f2d69a57bdc346b0f02cb33afe1ca3d01a3c
unknown <derrotalacrisisafiliadosblackb@gmail.com>
2019-09-17 12:29:40 +0100

    first commit

---
+++ src/main/java/dad/maven/hola/Main.java
@@ -0,0 +1,12 @@
+package dad.maven.hola;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String greeting =StringUtils.reverse("hola maven");
+		System.out.println(greeting);
+	}
+
+}
